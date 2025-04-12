f864cf0b9914900fb94fdb7e1af348c8692efd66
venkat080 <kasamramana@gmail.com>
2025-04-12 16:24:39 -0400

    Added_Goroutines

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"golangConcepts/models"
+	"time"
 )
 
 func main() {
@@ -19,4 +20,15 @@ func main() {
 	// Explicitly accessing the Draw method on the embedded Person field
 	fmt.Println(Ramana.Draw())  // Output: Drawing, Ramana
 	fmt.Println(Sandeep.Draw()) // Output: Drawing, Sandeep
+
+	go say("world")
+	say("hello")
+
+}
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
 }
